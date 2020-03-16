a42f702ee9e18ea06f54fccdfc074dd268e23d8e
Fabricio <fabricio.a.s@hotmail.com>
2020-03-16 15:37:26 -0300

    Initial commit
    Commit inicial do projeto start spring

---
+++ src/main/java/com/ravtecnologia/desafio/taskcontrol/TaskcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.ravtecnologia.desafio.taskcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravtecnologia/desafio/taskcontrol/TaskcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravtecnologia.desafio.taskcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
