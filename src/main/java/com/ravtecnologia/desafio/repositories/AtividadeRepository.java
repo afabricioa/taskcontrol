8999c2ee09cbfef478a94210e750de1210f79c37
Fabricio <fabricio.a.s@hotmail.com>
2020-03-16 16:44:53 -0300

    Implementação dos repositórios

---
+++ src/main/java/com/ravtecnologia/desafio/repositories/AtividadeRepository.java
@@ -0,0 +1,9 @@
+package com.ravtecnologia.desafio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ravtecnologia.desafio.models.Atividade;
+
+public interface AtividadeRepository extends JpaRepository<Atividade, Long>{
+
+}
---
+++ src/main/java/com/ravtecnologia/desafio/repositories/GrupoRepository.java
@@ -0,0 +1,9 @@
+package com.ravtecnologia.desafio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ravtecnologia.desafio.models.Grupo;
+
+public interface GrupoRepository extends JpaRepository<Grupo, Long>{
+
+}
