3e1ce577a238e9ac0ee3b23ed441f9ce244c1cef
Fabricio <fabricio.a.s@hotmail.com>
2020-03-16 16:04:52 -0300

    Estrutura inicial do projeto

--- src/main/java/com/ravtecnologia/desafio/TaskcontrolApplication.java
+++ src/main/java/com/ravtecnologia/desafio/TaskcontrolApplication.java
@@ -1,4 +1,4 @@
-package com.ravtecnologia.desafio.taskcontrol;
+package com.ravtecnologia.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
